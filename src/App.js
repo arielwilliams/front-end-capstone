317bb30864796cd03b0474ddb57102e342357b58
Divya <divya.naidoo5@gmail.com>
2023-07-27 15:50:50 -0700

    test button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import "./src/Button.js";
+import Button from "./Button.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button/>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,8 +18,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React */}
+        {/* </a> */}
       </header>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 function Button() {
   return <button type="button">{"This is a button"}</button>;
+
 }
 
 export default Button;
