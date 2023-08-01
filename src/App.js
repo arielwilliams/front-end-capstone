86465f41527b371fd74356e9df9c4ca05135f5de
Divya <divya.naidoo5@gmail.com>
2023-08-01 11:24:28 -0700

    edited code to be more readable and like Ada, moved it to Login.js, fixed button to show login again once logged out, created callback function in App that will set user using the info from Login, called Login in the App return and passed the callback function as a prop. it is working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { useEffect, useState } from "react";
-import jwt_decode from "jwt-decode";
+import { useState } from "react";
 
 // import page components
 import Home from "./pages/Home";
@@ -17,66 +16,34 @@ import Login from "./pages/Login";
 // created link tags for buttonswith path and name of each page (Home, Dashboard, Search, Logout)
 function App() {
 
-  const [ user, setUser ] = useState({});
+  const [user, setUser] = useState({})
 
-  function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    var userObject = jwt_decode(response.credential);
-    console.log(userObject);
-    setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
+  const userCallback = (userInfo) => {
+    setUser(userInfo);
   }
 
-
-  function handleSignOut(event) {
-    setUser({});
-    document.getElementById("signInDiv").hidden = true;
-  }
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large"}
-    )
-  }, []);
-
-  // if we have no user: sign in button
-  // if we have a user: show the log out button
+  console.log(user)
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div id="signInDiv"></div>
-        { Object.keys(user).length != 0 &&
-          <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
-        }
-    
-        { user && 
-          <div>
-            <img src={user.picture}></img>
-            <h3>{user.name}</h3>
-        </div>
-        }
+    <div>
+      <Login userCallback={userCallback}/>
+      <BrowserRouter>
+        <div className="App">
           <Navigation />
-        <main>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/logout" element={<Logout />} />
-            {/* <Route path="/login" element={<Login />} /> */}
-          </Routes>
-        </main>
-        <Button varName="I am a button"></Button>
-        <Button varName="Button #2"></Button>
-      </div>
-    </BrowserRouter>
+          <main>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/logout" element={<Logout />} />
+            </Routes>
+          </main>
+          <Button varName="I am a button"></Button>
+          <Button varName="Button #2"></Button>
+        </div>
+      </BrowserRouter>
+
+    </div>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,54 +1,54 @@
 import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 
-function Login() {
+const Login = ({userCallback}) => {
 
-const [ user, setUser ] = useState({});
+    const [ user, setUser ] = useState({});
 
-function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    var userObject = jwt_decode(response.credential);
-    console.log(userObject);
-    setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
-}
+    const handleCallbackResponse = (response) => {
+        console.log("Encoded JWT ID token: " + response.credential);
+        const userObject = jwt_decode(response.credential);
+        setUser(userObject);
+        userCallback(userObject)
+        document.getElementById("signInDiv").hidden = true;
+    }
 
-function handleSignOut(event) {
-    setUser({});
-    document.getElementById("signInDiv").hidden = true;
-}
+    const handleSignOut = (event) => {
+        setUser({});
+        userCallback({})
+        document.getElementById("signInDiv").hidden = false;
+    }
 
-useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-    client_id: "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
-    callback: handleCallbackResponse
-    });
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+        client_id: "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+        });
 
-    google.accounts.id.renderButton(
-    document.getElementById("signInDiv"),
-    { theme: "outline", size: "large"}
-    )
-}, []);
+        google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme: "outline", size: "large"}
+        )
+    }, []);
 
-// if we have no user: sign in button
-// if we have a user: show the log out button
+    // if we have no user: sign in button
+    // if we have a user: show the log out button
 
-return (
-    <div className="App">
-        <div id="signInDiv"></div>
-        { Object.keys(user).length !== 0 &&
-        <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
-        }
-    
-        { user && 
-        <div>
-            <img src={user.picture}></img>
-            <h3>{user.name}</h3>
+    return (
+        <div className="App">
+            <div id="signInDiv"></div>
+            { Object.keys(user).length !== 0 &&
+            <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+            }
+        
+            { Object.keys(user).length !== 0 &&
+            <div>
+                <h3>Hello {user.name}!</h3>
+            </div>
+            }
         </div>
-        }
-    </div>
-);
+    );
 }
 
 export default Login;
