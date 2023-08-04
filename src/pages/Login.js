ec76ad4fa58fe76f63af079d48af7b52f0a07288
Ariel Williams <arielwilliams121013@gmail.com>
2023-08-03 23:07:03 -0700

    Created form element and handleSearchFormSubmit func in S earch.js. Passed in event into the func and called preventDefault() so on form submit does not refresh the page and log the user out. In JSX, added onSubmit event. Used anonymous func so handleSearchFormSubmit would only g et called when the event of submission of the form occurs.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,54 +1,54 @@
 import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 
-const Login = ({userCallback}) => {
+const Login = ({ userCallback }) => {
+  const [user, setUser] = useState({});
 
-    const [ user, setUser ] = useState({});
+  const handleCallbackResponse = (response) => {
+    console.log("Encoded JWT ID token: " + response.credential);
+    const userObject = jwt_decode(response.credential);
+    setUser(userObject);
+    userCallback(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  };
 
-    const handleCallbackResponse = (response) => {
-        console.log("Encoded JWT ID token: " + response.credential);
-        const userObject = jwt_decode(response.credential);
-        setUser(userObject);
-        userCallback(userObject)
-        document.getElementById("signInDiv").hidden = true;
-    }
+  const handleSignOut = (event) => {
+    setUser({});
+    userCallback({});
+    document.getElementById("signInDiv").hidden = false;
+  };
 
-    const handleSignOut = (event) => {
-        setUser({});
-        userCallback({})
-        document.getElementById("signInDiv").hidden = false;
-    }
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id:
+        "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
 
-    useEffect(() => {
-        /* global google */
-        google.accounts.id.initialize({
-        client_id: "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
-        callback: handleCallbackResponse
-        });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
 
-        google.accounts.id.renderButton(
-        document.getElementById("signInDiv"),
-        { theme: "outline", size: "large"}
-        )
-    }, []);
+  // if we have no user: sign in button
+  // if we have a user: show the log out button
 
-    // if we have no user: sign in button
-    // if we have a user: show the log out button
+  return (
+    <div className="App">
+      <div id="signInDiv"></div>
+      {Object.keys(user).length !== 0 && (
+        <button onClick={(e) => handleSignOut(e)}>Logout</button>
+      )}
 
-    return (
-        <div className="App">
-            <div id="signInDiv"></div>
-            { Object.keys(user).length !== 0 &&
-            <button onClick={ (e) => handleSignOut(e)}>Logout</button>
-            }
-        
-            { Object.keys(user).length !== 0 &&
-            <div>
-                <h3>Hello {user.name}!</h3>
-            </div>
-            }
+      {Object.keys(user).length !== 0 && (
+        <div className="font-bold">
+          <h3>Hello {user.name}!</h3>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Login;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,19 +3,35 @@ import Button from "../components/Button";
 
 // notes: we will need an input field wrapped in form field and an event listener for on form submit
 // when user clicks the submit button then hit the yelp API once for those suggestions
-// if user wants to add a restaurant to their list they would have the option to create a new list or add to existing list.
+// if user wants to add a restaurant to their list, it would immediately get added to their favorites list.
 // THEN, that restaurant will get saved to the BE database.
 
 // UNLESS WE WANT SEARCH SUGGESTIONS (that would be on key down... which would probs be more work)
 
 const Search = () => {
+  const handleSearchFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("Hello");
+  };
+
   return (
     <>
       <h1 className="text-center font-semibold text-5xl m-6">
         Welcome to the Search Page
       </h1>
-      <input type="search" />
-      <button>Submit</button>
+      <h2 className="font-semibold">
+        Please enter any restaurant or key terms into the search field below.
+      </h2>
+      <form onSubmit={(event) => handleSearchFormSubmit(event)}>
+        <input
+          className="bg-blue-200 rounded-lg py-4 px-6 text-2xl focus:outline-none focus:ring focus:border-blue-300"
+          type="search"
+          required
+        />
+        <button className="bg-blue-500 text-white font-bold py-4 px-6 rounded ml-2">
+          Submit
+        </button>
+      </form>
     </>
   );
 };
