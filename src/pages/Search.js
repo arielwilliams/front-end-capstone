a137901c71f6121dea3363adc2db320d206d77ff
Ariel Williams <arielwilliams121013@gmail.com>
2023-08-07 15:56:09 -0700

    Tried making API call using fetch syntax. Currently not functional, getting a CORS error.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import Button from "../components/Button";
 
 const Search = () => {
+  const [input, setInput] = useState("");
+
   const handleSearchFormSubmit = (event) => {
     event.preventDefault();
-    console.log("Hello");
+    console.log("Hello", { event });
     console.log(process.env.REACT_APP_YELP_API_KEY);
     // fetchYelpData();
+    fetchData();
   };
 
   // make call with fetch syntax
@@ -15,6 +19,27 @@ const Search = () => {
   // approves access.
   // Then you need to take what's in the input field and ask the service to
   // search for that info
+
+  const fetchData = (value) => {
+    const options = {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.REACT_APP_YELP_API_KEY,
+        "Access-Control-Allow-Origin": "https://api.yelp.com/*",
+        mode: "cors",
+        "Access-Control-Allow-Headers": process.env.REACT_APP_YELP_API_KEY,
+      },
+    };
+    console.log(input);
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${input}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  };
 
   // const fetchYelpData = () => {
   //   fetch(YELP_API_KEY)
@@ -36,16 +61,45 @@ const Search = () => {
       </h2>
       <form onSubmit={(event) => handleSearchFormSubmit(event)}>
         <input
+          placeholder="Type to search..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
           className="bg-blue-200 rounded-lg py-4 px-6 text-2xl focus:outline-none focus:ring focus:border-blue-300"
           type="search"
           required
         />
-        <button className="bg-blue-500 text-white font-bold py-4 px-6 rounded ml-2">
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-bold py-4 px-6 rounded ml-2"
+        >
           Submit
         </button>
       </form>
     </>
   );
 };
+
+// WHAT THE JSX WAS BEFORE
+//   return (
+//     <>
+//       <h1 className="text-center font-semibold text-5xl m-6">
+//         Welcome to the Search Page
+//       </h1>
+//       <h2 className="font-semibold">
+//         Please enter any restaurant or key terms into the search field below.
+//       </h2>
+//       <form onSubmit={(event) => handleSearchFormSubmit(event)}>
+//         <input
+//           className="bg-blue-200 rounded-lg py-4 px-6 text-2xl focus:outline-none focus:ring focus:border-blue-300"
+//           type="search"
+//           required
+//         />
+//         <button className="bg-blue-500 text-white font-bold py-4 px-6 rounded ml-2">
+//           Submit
+//         </button>
+//       </form>
+//     </>
+//   );
+// };
 
 export default Search;
