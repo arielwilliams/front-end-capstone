48ad15119c00a43d549f6accf10e3baca1acce98
Ariel Williams <arielwilliams121013@gmail.com>
2023-08-09 15:06:39 -0700

    Updated fetchData function to make a GET request to the BE. Updated useState from searchResult to searchResults since we will want an array of objects to return if there are mulitple locations of a restaurant. Created helper function called sanitizeInput to control the request (whatever user types in) and the name in the response. This helper function accounts for making everything lowercase and trim to remove whitespace from the front and back of the input field.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -30,11 +30,12 @@ const Search = () => {
   // API call requires user location AND input. It throws error if these are missing.
   // If we want to search for a restaurant in a different location we need a location input field in the form
   const [userLocation, setUserLocation] = useState({});
-  const [searchResult, setSearchResult] = useState({});
+  const [searchResults, setSearchResults] = useState([]);
 
   const yelpUrl = {
     proxy: "https://cors-anywhere.herokuapp.com/",
     api: "https://api.yelp.com/v3/businesses/search",
+    backend: "https://jakd-backend-capstone.onrender.com/search",
   };
 
   // gets user's current location when the webpage loads
@@ -65,24 +66,52 @@ const Search = () => {
     fetchData();
   };
 
-  //
+  const sanitizeInput = (input) => {
+    return input.toLowerCase().trim();
+  };
+
+  ////// fetchData makes GET request to BE to grab response from yelp API ///////
   const fetchData = async () => {
     const options = {
       method: "GET",
-      mode: "cors",
       headers: {
         accept: "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
       },
     };
 
-    const apiURL = `${yelpUrl.proxy}${yelpUrl.api}?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&term=${input}`;
+    // .toLowerCase makes everything lowercase, .trim removes white space before and after search term
+    // these need to be in request and response in order to work properly
+    const apiURL = `${yelpUrl.backend}?term=${sanitizeInput(input)}`;
 
     await fetch(apiURL, options)
       .then((response) => response.json())
-      .then((response) => setSearchResult(response.businesses[0]));
+      .then((response) => {
+        setSearchResults(
+          response.businesses.filter((business) =>
+            sanitizeInput(business.name).includes(sanitizeInput(input))
+          )
+        );
+      });
   };
 
+  ////////////// FE calls yelp API (previous fetchData func) ////////////
+  // const fetchData = async () => {
+  //   const options = {
+  //     method: "GET",
+  //     mode: "cors",
+  //     headers: {
+  //       accept: "application/json",
+  //       Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+  //     },
+  //   };
+
+  //   const apiURL = `${yelpUrl.proxy}${yelpUrl.api}?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&term=${input}`;
+
+  //   await fetch(apiURL, options)
+  //     .then((response) => response.json())
+  //     .then((response) => setSearchResult(response));
+  // };
+  /////////////////////////////////////////////////////////////////////////
   return (
     <>
       <h1 className="text-center font-semibold text-5xl m-6">
@@ -107,26 +136,33 @@ const Search = () => {
           Submit
         </button>
       </form>
-      {Object.keys(searchResult).length > 0 && (
-        <section>
-          <h3>{searchResult.name}</h3>
-          <a href={"tel:" + searchResult.phone}>{searchResult.display_phone}</a>
-          <p>{searchResult.categories[0].title}</p>
+      {searchResults.length > 0 &&
+        searchResults.map((searchResult) => {
+          return (
+            <section className="pb-4">
+              <h3 className="text-lg">{searchResult.name}</h3>
+              <a href={"tel:" + searchResult.phone}>
+                {searchResult.display_phone}
+              </a>
+              <p>{searchResult.categories[0].title}</p>
 
-          <address>
-            <ul>
-              <li>{searchResult.location.address1}</li>
-              {searchResult.location.address2 !== "" && (
-                <li>{searchResult.location.address2}</li>
-              )}
-              <li>
-                <span>{searchResult.location.city} </span>
-                {searchResult.location.state}, {searchResult.location.zip_code}
-              </li>
-            </ul>
-          </address>
-        </section>
-      )}
+              <address>
+                <ul>
+                  <li>{searchResult.location.address1}</li>
+                  {searchResult.location.address2 !== "" && (
+                    <li>{searchResult.location.address2}</li>
+                  )}
+                  <li>
+                    <span>{searchResult.location.city} </span>
+                    {searchResult.location.state},{" "}
+                    {searchResult.location.zip_code}
+                  </li>
+                </ul>
+              </address>
+              {/* <button type="button">❤️</button> */}
+            </section>
+          );
+        })}
     </>
   );
 };
