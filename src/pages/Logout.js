b3a3fb09e16ecb0fe9bcdcb2bbe2aed3bbd4278f
Ariel Williams <arielwilliams121013@gmail.com>
2023-08-10 11:29:52 -0700

    Removed comments in Search.js. Commented out code in App.js, Logout.js, and Navigation.js to remove Logout from the navigation bar.

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,11 +1,11 @@
-import React from "react";
+// import React from "react";
 
-const Logout = () => {
-  return (
-    <h1 className="text-center font-semibold text-5xl m-6">
-      You have been logged out
-    </h1>
-  );
-};
+// const Logout = () => {
+//   return (
+//     <h1 className="text-center font-semibold text-5xl m-6">
+//       You have been logged out
+//     </h1>
+//   );
+// };
 
-export default Logout;
+// export default Logout;
