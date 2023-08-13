c98287e569830da47a5a5783c8be197ffc0fe6c5
Divya <divya.naidoo5@gmail.com>
2023-08-13 11:31:43 -0700

    added a delete restaurant function in List, modified fetchRestaurants to update state when called in deleteRestaurant. in RestaurantCard have onClick to delete the restaurant, I can see that it is being clicked and it is getting the restaurantID but it isn't actually deleting.

--- src/components/List.js
+++ src/components/List.js
@@ -1,38 +1,75 @@
 import React from "react";
 import { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
 import RestaurantCard from "./RestaurantCard";
 
 const List = ({ list }) => {
-    // const { listId } = useParams();
     const [restaurants, setRestaurants] = useState([]);
 
 
-    useEffect(() => {
-        const fetchRestaurants = async () => {
-            try {
-                const response = await fetch(`https://jakd-backend-capstone.onrender.com/dashboard/list/${list.listId}`);
-                if (!response.ok) {
-                    throw new Error("Network response was not okay");
-                }
-                const data = await response.json();
-                // console.log("LOOK HERE")
-                // console.log("fetched data:", data);
-                setRestaurants(data);
-                } catch (error) {
-                console.error("Error fetching restaurants:", error);
-                }
-        };
+    const fetchRestaurants = async () => {
+        try {
+            const response = await fetch(`https://jakd-backend-capstone.onrender.com/dashboard/list/${list.listId}`);
+            if (!response.ok) {
+                throw new Error("Network response was not okay");
+            }
+            const data = await response.json();
+            setRestaurants(data);
+            } catch (error) {
+            console.error("Error fetching restaurants:", error);
+            }
+    };
 
+    useEffect(() => {
         fetchRestaurants();
     }, [list]);
+
+
+    // useEffect(() => {
+    //     const fetchRestaurants = async () => {
+    //         try {
+    //             const response = await fetch(`https://jakd-backend-capstone.onrender.com/dashboard/list/${list.listId}`);
+    //             if (!response.ok) {
+    //                 throw new Error("Network response was not okay");
+    //             }
+    //             const data = await response.json();
+    //             // console.log("LOOK HERE")
+    //             // console.log("fetched data:", data);
+    //             setRestaurants(data);
+    //             } catch (error) {
+    //             console.error("Error fetching restaurants:", error);
+    //             }
+    //     };
+
+    //     fetchRestaurants();
+    // }, [list]);
+
+    const deleteRestaurant = (restaurantId) => {
+        fetch(`https://jakd-backend-capstone.onrender.com/dashboard/list/${list.listId}/${restaurantId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "applicatoin/json"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not okay");
+            }
+            return response.json();
+        })
+        .then(() => {
+            fetchRestaurants();
+        })
+        .catch((error) => 
+            console.log(error))
+    
+    };
 
     return (
         <div> 
             <h2>List Details:</h2>
             <ul> 
                 {restaurants.map((restaurant) => (
-                    <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+                    <RestaurantCard key={restaurant.id} restaurant={restaurant} deleteRestaurant={deleteRestaurant} />
             ))}
             </ul>
         </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,23 +3,26 @@
 import React from "react";
 
 
-const RestaurantCard = ({restaurant}) => {
+const RestaurantCard = ({restaurant, deleteRestaurant}) => {
+
+    const onDeleteClick = () => {
+        console.log("Delete button clicked for Restaurant:", restaurant.restaurantId);
+        deleteRestaurant(restaurant.restaurantId);
+    }
+
+
     return (
         <div className="restaurant-card">
             <h3>{restaurant.restaurantName}</h3>
             <p>Cuisine: {restaurant.cuisine}</p>
-            {/* <p>Rating: {restaurant.rating}</p> */}
             <p>Phone: {restaurant.phoneNumber}</p>
             <p>Price Range: {restaurant.pricePoint}</p>
             <p>Address: {restaurant.address}</p>
-            {/* <p>Categories: {restaurant.categories.join(', ')}</p> */}
-            {/* <img src={restaurant.image_url} alt={restaurant.restaurantName} /> */}
+            <button onClick={onDeleteClick}>Delete Restaurant</button>
+            {/* <button onClick= {() => deleteRestaurant(restaurant.id)}> Delete Restaurant</button> */}
         </div>
     );
 };
 
 
 export default RestaurantCard;
-
-
-
