ecbb3eb67d3282839201ae48fad1e1a78c44fa07
Divya <divya.naidoo5@gmail.com>
2023-08-14 14:45:11 -0700

    deleted unused imports and comments in AllLists

--- src/components/AllLists.js
+++ src/components/AllLists.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { useState } from "react";
-// import { Route, Routes, useNavigate } from "react-router-dom";
 import List from "./List";
 
 
 const AllLists = ({ lists }) => {
 
-    // const navigate = useNavigate();
-
     const [selectedList, setSelectedList] = useState(null);
 
     const handleListClick = (list) => {
         setSelectedList(list);
-        // navigate(`/list/${listId}`);
     };
 
 
@@ -23,13 +19,9 @@ const AllLists = ({ lists }) => {
                 {lists.map((list) => (
                     <li key={list.listId}>
                         <button onClick={() => handleListClick(list)}>{list.name}</button>
-                        {/* <Link to={`/dashboard/list/${list.listId}`}>{list.name}</Link> */}
                     </li>
                 ))}
             </ul>
-            {/* <Routes>
-                <Route path="/list/:listId" element={<List />} />
-            </Routes> */}
             {selectedList && <List list={selectedList} />}
         </div>
     );
