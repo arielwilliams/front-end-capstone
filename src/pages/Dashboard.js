3768bf82211a38127557a070d11e0beb54cd7741
Divya <divya.naidoo5@gmail.com>
2023-08-14 14:46:54 -0700

    deleted comments and unused imports from Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import { Route, Routes } from "react-router-dom";
 import AllLists from "../components/AllLists";
-// import List from "../components/List";
 
 const Dashboard = (props) => {
   if (Object.keys(props.user).length > 0) {
