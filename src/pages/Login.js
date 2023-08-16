c2435715d1a9ff9890d76fb0eeb520b44a152525
Janice <janicenguyenn@gmail.com>
2023-08-15 18:54:17 -0700

    fixed bug where user gets saved to database everytime they log in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
+
 const Login = ({ userCallback }) => {
   const [user, setUser] = useState({});
+  const [userChecked, setUserChecked] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleCallbackResponse = (response) => {
     console.log("Encoded JWT ID token: " + response.credential);
@@ -13,95 +15,89 @@ const Login = ({ userCallback }) => {
     setUser(userObject);
     userCallback(userObject);
     document.getElementById("signInDiv").hidden = true;
-    navigate("/dashboard")
-    console.log(userObject)
-    // api call to the backend, post the user info including name and picture and sub ID 
-    // userObject.result.sub
-    // logic in the api call in the backend to check if that ID is already in the database
-    // if it is, terminate API. if it has not, post it 
-    fetch(`https://jakd-backend-capstone.onrender.com/dashboard/user/users`)
-    .then(response => response.json()) 
-    .then(data => {
-      try {
-        const userId = userObject.sub; // Extract the sub ID
-        const userExists = data.some(user => user.subId === userId);
-        const userName = userObject.name;
-        const userGivenName = userObject.given_name;
-        const userEmail = userObject.email;
-        const profilePicture = userObject.picture;
-  
-        if (userExists) {
-          console.log("User exists");
-        } else {
-          // Save the user to the database
-          fetch(`https://jakd-backend-capstone.onrender.com/dashboard/user/save-user`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ subId: userId ,givenName: userGivenName, email: userEmail, name: userName, picture: profilePicture}),
-          })
-          .then(response => response.json())
-          .then(result => {
-            console.log("User saved sucessfully:", result);
-          })
-          .catch(error => {
-            console.log("Error saving user:", error);
-          });
-        }
-      } catch (error) {
-        console.log("Error processing user data:", error);
-      }
-    })
-    .catch(error => {
-      console.log("Error fetching data:", error);
-    });
-    
+    navigate("/dashboard");
+    console.log(userObject);
   };
 
   const handleSignOut = (event) => {
     setUser({});
     userCallback({});
     document.getElementById("signInDiv").hidden = false;
-    navigate("/home")
+    navigate("/home");
   };
 
+   /* global google */
   useEffect(() => {
-    /* global google */
+    google.accounts.id.initialize({
+      client_id: "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
 
-    if (typeof google !== "undefined" && google.accounts) {
-      google.accounts.id.initialize({
-        client_id:
-          "865937179776-r5timpp1f57epgi3q06blrv8ftvu5qev.apps.googleusercontent.com",
-        callback: handleCallbackResponse,
-      });
-  
-      google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-        theme: "outline",
-        size: "large",
-      });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(user).length !== 0 && !userChecked) {
+      setUserChecked(true);
+
+      fetch(`https://jakd-backend-capstone.onrender.com/dashboard/user/users`)
+        .then((response) => response.json())
+        .then((data) => {
+          try {
+            const userId = user.sub; 
+            const userExists = data.some((user) => user.subId === userId);
+
+            if (userExists) {
+              console.log("User exists");
+            } else {
+              fetch(`https://jakd-backend-capstone.onrender.com/dashboard/user/save-user`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  subId: userId,
+                  givenName: user.given_name,
+                  email: user.email,
+                  name: user.name,
+                  picture: user.picture,
+                }),
+              })
+                .then((response) => response.json())
+                .then((result) => {
+                  console.log("User saved successfully:", result);
+                })
+                .catch((error) => {
+                  console.log("Error saving user:", error);
+                });
+            }
+          } catch (error) {
+            console.log("Error processing user data:", error);
+          }
+        })
+        .catch((error) => {
+          console.log("Error fetching data:", error);
+        });
     }
-    }, []);
-
-  // if we have no user: sign in button
-  // if we have a user: show the log out button
+  }, [user, userChecked]);
 
   return (
     <div className="App">
       <div id="signInDiv"></div>
-      <>
-        {Object.keys(user).length !== 0 && (
-          <button onClick={(e) => handleSignOut(e)}>Logout</button>
-        )}
-      </>
-
       {Object.keys(user).length !== 0 && (
-        <div className="font-bold">
-          <h3>Hello, {user.name}!</h3>
-        </div>
+        <>
+          <button onClick={(e) => handleSignOut(e)}>Logout</button>
+          <div className="font-bold">
+            <h3>Hello, {user.name}!</h3>
+          </div>
+        </>
       )}
     </div>
   );
 };
 
 export default Login;
+
