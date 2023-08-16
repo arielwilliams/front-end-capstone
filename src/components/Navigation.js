3dc387a1095d74e25368d010ceb6bdc82f12e430
Ariel Williams <arielwilliams121013@gmail.com>
2023-08-16 11:49:53 -0700

    Updated navigation bar to be emerald-900 and on hover emerald-700.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,22 +5,22 @@ const Navigation = (props) => {
   if (Object.keys(props.user).length > 0) {
     return (
       <>
-        <nav className="flex justify-between bg-blue-500 p-4">
+        <nav className="flex justify-between bg-emerald-900 p-4">
           <Link
             to="/home"
-            className="text-white hover:bg-blue-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
           >
             Home
           </Link>
           <Link
             to="/dashboard"
-            className="text-white hover:bg-blue-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
           >
             Dashboard
           </Link>
           <Link
             to="/search"
-            className="text-white hover:bg-blue-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
           >
             Search
           </Link>
