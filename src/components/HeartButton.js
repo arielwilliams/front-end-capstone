1b94f6767bcc6c33e6641e8f2ec69c0a726daea1
Ariel Williams <arielwilliams121013@gmail.com>
2023-08-16 16:34:43 -0700

    Declared props in Search.js. In App.js, passed in list and setListData as props into the Search component. Created HeartButton component that is a child to Search in order to dynamically set state per button. In HeartButton, created useState to check if favorite list element has the the searchResult.id. This isDisabled, setIsDisabled useState will return a boolean depending on if the restaurant is in the list or not. UseState determines if the heart should be filled in or not. If the heart is filled in, the heart button is disabled. If you can click the button to add it to your favorites you update setListData and setIsDisabled to be true.

---
+++ src/components/HeartButton.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function HeartButton({ list, searchResult, setListData }) {
+  // Check to see if favorite list element has the searchResult.id
+  // We force with !! to be type of boolean
+  const [isDisabled, setIsDisabled] = useState(
+    !!list.find((element) => element.yelpId === searchResult.id)
+  );
+
+  const saveFavoriteUrl =
+    "https://jakd-backend-capstone.onrender.com/search/save-favorite";
+
+  const handleClickAddToFavoriteList = async (searchResult) => {
+    await postDataToFavList(searchResult);
+  };
+
+  const postDataToFavList = async (searchResult) => {
+    console.log("Data to be sent: ", searchResult);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(searchResult),
+    };
+
+    try {
+      const response = await fetch(saveFavoriteUrl, options);
+      if (response.ok) {
+        setListData(list.concat(searchResult));
+        setIsDisabled(true);
+        console.log("Restaurant saved to favorites");
+      } else {
+        console.error("Failed to save restaurant to favorites");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  return (
+    <button
+      onClick={() => handleClickAddToFavoriteList(searchResult)}
+      type="button"
+      disabled={isDisabled}
+    >
+      {isDisabled ? <>♥️</> : <>♡</>}
+    </button>
+  );
+}
+
+export default HeartButton;
