b9931e6678b35ed5257b33c193115fbe5f7d357c
Janice <janicenguyenn@gmail.com>
2023-08-16 21:13:33 -0700

    add styling to dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -32,18 +32,18 @@ import AllLists from "../components/AllLists";
 const Dashboard = ({ list, user, setListData }) => {
   if (Object.keys(user).length > 0) {
     return (
-      <div className="relative bg-cover bg-no-repeat bg-center min-h-[100vh] py-4" style={{ backgroundImage: `url('/dashboard_image.jpg')` }}>
-        <h1 className="text-center font-semibold text-5xl m-6 text-white">
+      <div className="relative bg-cover bg-no-repeat bg-center min-h-[100vh] py-4 fter:opacity-[.30]" style={{ backgroundImage: `url('/pasta.jpg')` }}>
+        <h1 className="text-center font-semibold text-5xl m-6 text-green-900">
           Welcome to your Dashboard!
         </h1>
-        <h2 className="text-center font-light text-white">
+        <h2 className="text-center font-light text-black">
           On your Dashboard you can view all the restaurants you want to try and
           manage your favorites. You can get started by heading to the Search
           page to add restaurants to your favorites list!
         </h2>
-        <h2 className="text-left font-bold text-2xl ml-6 text-white">
+        <h3 className="text-left font-bold text-2xl ml-6 text-green-900">
           {user.given_name}'s Lists
-        </h2>
+        </h3>
         <AllLists list={list} setListData={setListData} />
       </div>
     );
