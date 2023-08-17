0c2cf30f7795d3c527177275a7fab3b97b1a0133
kathleen <kathleen.n.shin@gmail.com>
2023-08-17 11:51:06 -0700

    Element formatting with all images aligned

--- src/pages/About.js
+++ src/pages/About.js
@@ -202,6 +202,82 @@
 
 
 
+// import React from "react";
+// import aboutUsData from "../data/about-us.js";
+
+// const About = () => {
+//   const aboutImage =
+//     "https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80";
+
+//   return (
+//     <div
+//       className="bg-cover bg-center relative"
+//       style={{ backgroundImage: `url(${aboutImage})` }}
+//     >
+//       <div
+//         className="bg-black bg-opacity-50 p-8"
+//         style={{ maxWidth: "1200px", margin: "0 auto" }}
+//       >
+//         <h1 className="text-center font-semibold text-5xl text-white mb-6">
+//           About Us
+//         </h1>
+//         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+//           {aboutUsData.map((element) => (
+//             <div
+//               key={element.id}
+//               className="border p-4 rounded shadow bg-white bg-opacity-100 flex"
+//             >
+//               {element.image && (
+//                 <img
+//                   src={element.image}
+//                   alt={element.name}
+//                   className="rounded-full w-16 h-16 md:w-24 md:h-24 mr-4"
+//                 />
+//               )}
+//               <div className="flex flex-col justify-between">
+//                 <div>
+//                   <h2 className="text-xl font-semibold text-emerald-900">
+//                     {element.name}
+//                   </h2>
+//                   <p className="text-emerald-950">{element.blurb}</p>
+//                 </div>
+//                 {element.github || element.linkedin ? (
+//                   <div className="mt-2">
+//                     {element.github && (
+//                       <a
+//                         target="_blank"
+//                         rel="noopener noreferrer"
+//                         href={element.github}
+//                         className="text-blue-500 hover:text-blue-600 mr-2"
+//                       >
+//                         GitHub
+//                       </a>
+//                     )}
+//                     {element.linkedin && (
+//                       <a
+//                         target="_blank"
+//                         rel="noopener noreferrer"
+//                         href={element.linkedin}
+//                         className="text-blue-500 hover:text-blue-600"
+//                       >
+//                         LinkedIn
+//                       </a>
+//                     )}
+//                   </div>
+//                 ) : null}
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
+
+
+
 import React from "react";
 import aboutUsData from "../data/about-us.js";
 
@@ -219,7 +295,7 @@ const About = () => {
         style={{ maxWidth: "1200px", margin: "0 auto" }}
       >
         <h1 className="text-center font-semibold text-5xl text-white mb-6">
-          About Us
+          About
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {aboutUsData.map((element) => (
@@ -227,13 +303,16 @@ const About = () => {
               key={element.id}
               className="border p-4 rounded shadow bg-white bg-opacity-100 flex"
             >
-              {element.image && (
-                <img
-                  src={element.image}
-                  alt={element.name}
-                  className="rounded-full w-16 h-16 md:w-24 md:h-24 mr-4"
-                />
-              )}
+              <div className="w-24 h-24 md:w-24 md:h-24 mr-4 flex-shrink-0">
+                {element.image && (
+                  <img
+                    src={element.image}
+                    alt={element.name}
+                    className="rounded-full w-full h-full"
+                    style={{ objectFit: "cover" }}
+                  />
+                )}
+              </div>
               <div className="flex flex-col justify-between">
                 <div>
                   <h2 className="text-xl font-semibold text-emerald-900">
@@ -275,3 +354,7 @@ const About = () => {
 };
 
 export default About;
+
+
+
+
