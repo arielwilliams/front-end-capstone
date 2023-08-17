42d32f7f9c814fe0d3c3237924a69dedc17606da
Divya <divya.naidoo5@gmail.com>
2023-08-17 11:33:23 -0700

    modified the navigation to have the font be bolder and bigger

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,25 +10,25 @@ const Navigation = (props) => {
         <nav className="flex justify-between bg-emerald-900 p-4">
           <Link
             to="/home"
-            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded font-black text-lg"
           >
             Home
           </Link>
           <Link
             to="/about"
-            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded font-black text-lg"
           >
             About
           </Link>
           <Link
             to="/dashboard"
-            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded font-black text-lg"
           >
             Dashboard
           </Link>
           <Link
             to="/search"
-            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded font-black text-lg"
           >
             Search
           </Link>
@@ -41,13 +41,13 @@ const Navigation = (props) => {
         <div className="flex">
           <Link
             to="/home"
-            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded font-black text-lg"
           >
             Home
           </Link>
           <Link
             to="/about"
-            className="text-white hover:bg-emerald-700 px-4 py-2 rounded"
+            className="text-white hover:bg-emerald-700 px-4 py-2 rounded font-black text-lg"
           >
             About
           </Link>
