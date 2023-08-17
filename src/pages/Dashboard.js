cb8ad35eb5d990875a87c6f803ce0f510e6cced5
kathleen <kathleen.n.shin@gmail.com>
2023-08-16 17:12:04 -0700

    Added background image

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,20 +1,47 @@
+// import React from "react";
+// import AllLists from "../components/AllLists";
+
+// const Dashboard = ({ list, user, setListData }) => {
+//   if (Object.keys(user).length > 0) {
+//     return (
+//       <div>
+//         <h1 className="text-center font-semibold text-5xl m-6">
+//           {" "}
+//           Welcome to your Dashboard!
+//         </h1>
+//         <h2 className="text-center font-light">
+//           On your Dashboard you can view all the restaurants you want to try and
+//           manage your favorites. You can get started by heading to the Search
+//           page to add restaurants to your favorites list!
+//         </h2>
+//         <h2 className="text-left font-bold text-2xl ml-6">
+//           {user.given_name}'s Lists
+//         </h2>
+//         <AllLists list={list} setListData={setListData} />
+//       </div>
+//     );
+//   }
+// };
+
+// export default Dashboard;
+
+
 import React from "react";
 import AllLists from "../components/AllLists";
 
 const Dashboard = ({ list, user, setListData }) => {
   if (Object.keys(user).length > 0) {
     return (
-      <div>
-        <h1 className="text-center font-semibold text-5xl m-6">
-          {" "}
+      <div className="relative bg-cover bg-no-repeat bg-center min-h-[100vh] py-4" style={{ backgroundImage: `url('/dashboard_image.jpg')` }}>
+        <h1 className="text-center font-semibold text-5xl m-6 text-white">
           Welcome to your Dashboard!
         </h1>
-        <h2 className="text-center font-light">
+        <h2 className="text-center font-light text-white">
           On your Dashboard you can view all the restaurants you want to try and
           manage your favorites. You can get started by heading to the Search
           page to add restaurants to your favorites list!
         </h2>
-        <h2 className="text-left font-bold text-2xl ml-6">
+        <h2 className="text-left font-bold text-2xl ml-6 text-white">
           {user.given_name}'s Lists
         </h2>
         <AllLists list={list} setListData={setListData} />
@@ -24,3 +51,5 @@ const Dashboard = ({ list, user, setListData }) => {
 };
 
 export default Dashboard;
+
+
