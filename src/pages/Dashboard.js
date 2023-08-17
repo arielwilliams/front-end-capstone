6cbe81f8d66d7c44266c7415b33d0eae0c0d9144
Janice <janicenguyenn@gmail.com>
2023-08-16 22:13:51 -0700

    created section for h1 and h3

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -33,6 +33,7 @@ const Dashboard = ({ list, user, setListData }) => {
   if (Object.keys(user).length > 0) {
     return (
       <div className="relative bg-cover bg-no-repeat bg-center min-h-[100vh] py-4 fter:opacity-[.30]" style={{ backgroundImage: `url('/pasta.jpg')` }}>
+       <section className="rounded p-4 w-full border-black bg-white">
         <h1 className="text-center font-semibold text-5xl m-6 text-green-900">
           Welcome to your Dashboard!
         </h1>
@@ -41,10 +42,18 @@ const Dashboard = ({ list, user, setListData }) => {
           manage your favorites. You can get started by heading to the Search
           page to add restaurants to your favorites list!
         </h2>
-        <h3 className="text-left font-bold text-2xl ml-6 text-green-900">
-          {user.given_name}'s Lists
-        </h3>
-        <AllLists list={list} setListData={setListData} />
+       
+  <div className="bg-white rounded p-4">
+    <h3 className="font-bold text-2xl ml-6 text-green-900 w-full flex justify-center bg-white rounded p-4">
+      {user.given_name}'s Lists
+    </h3>
+  </div>
+ </section >
+ <section className="flex justify-center font-bold text-white">
+  <div className="text-white"> {/* Apply the desired text color here */}
+    <AllLists list={list} setListData={setListData} />
+  </div>
+</section>
       </div>
     );
   }
