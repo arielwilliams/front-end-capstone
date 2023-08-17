4646d3b4132212d5d57d4a57d0ff2e6fd1a85454
Divya <divya.naidoo5@gmail.com>
2023-08-16 20:05:59 -0700

    filled in info for about us and styled the page

--- src/data/about-us.js
+++ src/data/about-us.js
@@ -5,31 +5,26 @@ const aboutUsData = [
     blurb: "Welcome to Eatery Ease, where the intersection of passion for exploring new restaurants and organization comes together. This website embodies the culmination of my journey and those of my teammates at Ada Developers Academy, representing our capstone project. When it comes to the culinary realm, nothing beats indulging in an omakase sushi dinner. In Seattle, my favorite restaurant to dine at is Sushi Kashiba. For a more detailed look into my professional endeavors, feel free to connect with me on LinkedIn and explore my projects on GitHub. Thank you for being a part of my journey and exploring Eatery Ease",
     github: "https://github.com/arielwilliams",
     linkedin: "https://linkedin.com/in/ariel-w-827228ab",
-    image: "/logo512.png",
+
   },
   {
     id: 2,
     name: "Divya Naidoo",
     blurb: "Our team was brought together by a shared love of food, and this full-stack project has been an incredible learning experience and a great way to wrap up our time at Ada. My restaurant recommendations are Momiji for great sushi, or Harvest Vine for tapas.",
-    github: "https://www.github.com",
-    linkedin: "https://www.linkedin.com",
-    image: "/logo512.png",
+    github: "https://github.com/dnai55",
+    linkedin: "https://www.linkedin.com/in/divya-naidoo-1044b1163/",
   },
   {
     id: 3,
     name: "Janice Nguyen",
-    blurb: "I like food",
-    github: "https://www.github.com",
-    linkedin: "https://www.linkedin.com",
-    image: "/logo512.png",
+    blurb: "Boba enthusiast",
+    github: "https://github.com/janice15",
   },
   {
     id: 4,
     name: "Kathleen Shin",
-    blurb: "I like food",
-    github: "https://www.github.com",
-    linkedin: "https://www.linkedin.com",
-    image: "/logo512.png",
+    blurb: "Foodies unite! Our collective passion for food and sharing the best eats led us to create this platform for all food lovers to enjoy. P.S. If you’re ever in the Seattle Chinatown-International District, be sure to try out my favorite combo at Hood Famous Cafe + Bar: an iced ube latte and slice of ube cheesecake!",
+    linkedin: "https://www.linkedin.com/in/kathleen-shin/",
   },
 ];
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,124 @@
+// import React from "react";
+// import aboutUsData from "../data/about-us.js";
+
+// const About = () => {
+
+//     const aboutImage = "https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+// return (
+// <div
+//     className="bg-cover bg-center relative"
+//     style={{ backgroundImage: `url(${aboutImage})` }}
+//     >
+//     <div className="bg-black bg-opacity-50 p-8">
+//     <h1 className="text-center font-semibold text-5xl text-white mb-6">
+//         About
+//     </h1>
+//     <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+//         {aboutUsData.map((element) => (
+//         <li key={element.id} className="border p-4 my-4 rounded shadow bg-white bg-opacity-90">
+//             {element.image && (
+//             <img
+//                 src={element.image}
+//                 alt={element.name}
+//                 className="mx-auto mb-4"
+//             />
+//             )}
+//             <h2 className="text-xl font-semibold text-emerald-900">
+//             {element.name}
+//             </h2>
+//             <p className="text-emerald-900">{element.blurb}</p>
+//             {element.github || element.linkedin ? (
+//             <p>
+//                 {element.github && (
+//                 <a
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                     href={element.github}
+//                     className="text-blue-500 hover:text-blue-600 mr-2"
+//                 >
+//                     GitHub
+//                 </a>
+//                 )}
+//                 {element.linkedin && (
+//                 <a
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                     href={element.linkedin}
+//                     className="text-blue-500 hover:text-blue-600"
+//                 >
+//                     LinkedIn
+//                 </a>
+//                 )}
+//             </p>
+//             ) : null}
+//         </li>
+//         ))}
+//     </ul>
+//     </div>
+// </div>
+// );
+// };
+
+// export default About;
+
 import React from "react";
 import aboutUsData from "../data/about-us.js";
 
 const About = () => {
+const aboutImage = "https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80";
+
 return (
-<>
-    <h1 className="text-center font-semibold text-5xl m-6">
-    About
+<div
+    className="bg-cover bg-center relative"
+    style={{ backgroundImage: `url(${aboutImage})` }}
+>
+    <div className="bg-black bg-opacity-50 p-8">
+    <h1 className="text-center font-semibold text-5xl text-white mb-6">
+        About
     </h1>
-    <p>we like food!</p>
-    <ul>
-    {aboutUsData.map((element) => (
-        <li key={element.id}>
-        <img src={element.image} />
-        <h2>{element.name}</h2>
-        <p>{element.blurb}</p>
-        <p>
-            <a target="_blank" href={element.github}>
-            GitHub
-            </a>
-            <br />
-            <a target="_blank" href={element.linkedin}>
-            LinkedIn
-            </a>
-        </p>
-        </li>
-    ))}
-    </ul>
-</>
+    <div className="grid grid-cols-1 gap-4">
+        {aboutUsData.map((element) => (
+        <div key={element.id} className="border p-4 rounded shadow bg-white bg-opacity-90">
+            {element.image && (
+            <img
+                src={element.image}
+                alt={element.name}
+                className="mx-auto mb-4"
+            />
+            )}
+            <h2 className="text-xl font-semibold text-emerald-900">
+            {element.name}
+            </h2>
+            <p className="text-emerald-900">{element.blurb}</p>
+            {element.github || element.linkedin ? (
+            <p>
+                {element.github && (
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={element.github}
+                    className="text-blue-500 hover:text-blue-600 mr-2"
+                >
+                    GitHub
+                </a>
+                )}
+                {element.linkedin && (
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={element.linkedin}
+                    className="text-blue-500 hover:text-blue-600"
+                >
+                    LinkedIn
+                </a>
+                )}
+            </p>
+            ) : null}
+        </div>
+        ))}
+    </div>
+    </div>
+</div>
 );
 };
 
